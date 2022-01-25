fc85db25ac0dff439bcaad7a0e40ede414d1ca7b
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-01-25 17:32:22 +0000

    now returns blank entries for miss data

--- src/components/WorkDetails.jsx
+++ src/components/WorkDetails.jsx
@@ -4,17 +4,17 @@ const WorkDetails = ({ work }) => {
     let subjectString;
     if (work.hasOwnProperty("subjects")) {
       subjectString = work.subjects.join(", ");
-      console.log(subjectString);
     }
     // gather available fields ------------------------------
     let fields = ["first_publish_date", "pages", "description"];
     const foundFields = [subjectString];
-    console.log(foundFields);
 
     for (let i = 0; i < fields.length; i++) {
       const field = fields[i];
       if (work.hasOwnProperty(field)) {
         foundFields.unshift(work[field]);
+      } else {
+        foundFields.unshift("n/a");
       }
     }
     //return ------------------------------------------------------------

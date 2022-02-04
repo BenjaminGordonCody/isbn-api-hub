8bc1012fa258ae11faf8d93a44297b2ddfe5e77f
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-04 16:50:50 +0000

    updating file names for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import bookArrFromISBNArr from "./funcs/BookObj";
+import bookArrFromISBNArr from "./funcs/ISBNConversionFuncs";
 
 function App() {
   //states ------------------------------------------------------

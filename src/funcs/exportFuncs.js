80e8dd5a41c2b076c1f40f27a660db8820157482
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-21 14:06:35 +0000

    improved error handling

--- src/funcs/exportFuncs.js
+++ src/funcs/exportFuncs.js
@@ -1,9 +1,10 @@
 export const makeSummaryObject = (edition, work, author) => {
   try {
     return {
-      title: edition.title,
-      author: author.name,
+      title: edition.title ? edition.title : null,
+      author: author.name ? author.name : null,
       tags: work.subjects ? work.subjects.join(", ") : null,
+      description: work.description.value ? work.description.value : null,
     };
   } catch (e) {
     return {};
